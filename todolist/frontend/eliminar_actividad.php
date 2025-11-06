bd1420d82b9a8b525100e3b7ee98c89854a7a130
YadiiCabeza96 <yadicabeza12.96@gmail.com>
2025-11-06 15:12:24 -0500

    1

--- todolist/frontend/eliminar_actividad.php
+++ todolist/frontend/eliminar_actividad.php
@@ -11,25 +11,30 @@
     </head>
     <body>
         <div class="container cont_todolist">
-            <h1 class="text-center">MOSTAR  ACTIVIDAD</h1>
+            <h1 class="text-center">ELIMINAR ACTIVIDAD</h1>
          <a href="index.html" class="btn btn-primary">Regresar al index</a>
-         <div class="container documento_de_actividad" >
-            <h1 class="text-center"> <strong>Actividad:</strong> <span id="actividad">Deberes de calculo</span></h1>
+         <div class="container fomulario_actividad" >
+            <p> <strong>Actividad:</strong> <span id="actividad">Deberes de calculo</span></p>
             <p> <strong>Descripción:</strong> <span id="descripcion">Deberes de calculo</span></p>
             <p> <strong>Estado:</strong> <span id="estado">Completado</span></p>
             <p> <strong>Fecha de Creación:</strong> <span id="fecha_creacion">2025-10-22</span></p>
             <p> <strong>Fecha de Actualización:</strong> <span id="fecha_actualizacion">2025-10-22</span></p>
            
          </div>
+         <form action="" method="post" id="formulario_eliminar_actividad">
+            <input type="hidden" name="id" id="id" value="<?php echo $_GET['id']; ?>">
+            <input type="submit" class="btn btn-danger" value="Eliminar Actividad">
+         </form>
         </div>
-        
-        <!-- jQuery -->
-        <script src="js/jquery-3.7.1.min.js"></script>
-        <!-- Bootstrap JS -->
+        <script src="js/jquery-3.7.1.min.js" ></script>
         <script src="css/bootstrap-5.3.8-dist/js/bootstrap.bundle.min.js"></script>
         <!-- SweetAlert2 JS -->
         <script src="https://cdn.jsdelivr.net/npm/sweetalert2@11"></script>
-        <!-- Main JS -->
         <script src="js/main.js"></script>
+    <script>
+        document.addEventListener('DOMContentLoaded', function () {
+            EliminarActividad();
+        });
+    </script>
     </body>
 </html>
